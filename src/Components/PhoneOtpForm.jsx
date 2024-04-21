b4b5b83bc5bd700f4f9376ab4f31387a0accbe37
Keerthana <keerthiram2612@gmail.com>
2024-04-21 13:24:49 +0530

    setting of the regex with the validations

--- src/Components/PhoneOtpForm.jsx
+++ src/Components/PhoneOtpForm.jsx
@@ -3,19 +3,34 @@ import React, { useState } from 'react'
 function PhoneOtpform() {
 
   const [phoneNumber,setPhoneNumber]=useState("")
+  const [showOtp,setShowOtp]=useState(false)
+
   const handlePhoneNumber=(event)=>{
     setPhoneNumber(event.target.value);
   }
-  const handlePhoneSubmit=()=>{
+  const handlePhoneSubmit=(event)=>{
+    event.preventDefault();
+
+    //phone validations
+    const regex = /[^0-9]/g;
+    if(phoneNumber.length<10|| regex.test(phoneNumber)){
+      alert("Invalid Phone Number");
+      return;
+    }
+    //call api
+    //show otp field
+    setShowOtp(true);
 
   }
   return (
     <div>
-      <form onSubmit={()=>{}}>
+      {!showOtp ?(
+      <form onSubmit={handlePhoneSubmit}>
         <input type='text' value={phoneNumber}
-        onChange={handlePhoneNumber} placeholder={"Enter Phone Number"}></input>
-        <button type="submit"></button>
-      </form>
+        onChange={handlePhoneNumber} placeholder="Enter Phone Number"></input>
+        <button type="submit">Submit</button>
+      </form>):(<div>
+        <p>Enter OTP sent to {phoneNumber} </p></div>)}
     </div>
   )
 }
