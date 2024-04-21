5d51d1cd9272dd4f978e863cf49a872f87868b76
Keerthana <keerthiram2612@gmail.com>
2024-04-21 12:58:50 +0530

    adding the default variable

--- src/Components/PhoneOtpForm.jsx
+++ src/Components/PhoneOtpForm.jsx
@@ -3,14 +3,18 @@ import React, { useState } from 'react'
 function PhoneOtpform() {
 
   const [phoneNumber,setPhoneNumber]=useState("")
-  const handlePhoneNumber=()=>{
-    
+  const handlePhoneNumber=(event)=>{
+    setPhoneNumber(event.target.value);
+  }
+  const handlePhoneSubmit=()=>{
+
   }
   return (
     <div>
       <form onSubmit={()=>{}}>
         <input type='text' value={phoneNumber}
         onChange={handlePhoneNumber} placeholder={"Enter Phone Number"}></input>
+        <button type="submit"></button>
       </form>
     </div>
   )
